746ecfd1bfd1f49f5fc2f939c3ea36a3d07706c6
kvatt1 <161676573+kvatt1@users.noreply.github.com>
2024-04-27 18:37:45 -0400

    Initial Commit

--- krishnavenivattikonda_vue/src/App.vue
+++ krishnavenivattikonda_vue/src/App.vue
@@ -1,26 +1,79 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <!--Step 3 use the component-->
+  <MyHeader yourName="krishnaveni Vattikonda"/> 
+  <MedicineBox :medicines=medicines />
+ 
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// Step: 1 Import the components
+import MyHeader from './components/MyHeader.vue'
+import MedicineBox from './components/MedicineBox.vue'
 
-export default {
+
+export default{
   name: 'App',
-  components: {
-    HelloWorld
+
+  // Step 2: Register the components
+  components:{
+    MyHeader,
+    MedicineBox
+  },
+  data(){
+    return {
+      medicines: []
+    }
+  },
+  methods:{
+    //promoises
+    async fetchmedicines(){
+      const res = await fetch('https://krishnavenivattikonda-node.onrender.com/api');
+      const data = await res.json()
+      console.log(data)
+      return data
+    }
+  },
+  async created(){
+      this.medicines = await this.fetchmedicines();
   }
+
+  // data() => this.data medicines
+  // method => this.method => fetchmedicine
+  // life cycle hooks created() => call our method here
+
 }
 </script>
 
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap');
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
 }
+
+body {
+  font-family: 'Montserrat', sans-serif;
+}
+.container {
+  max-width: 400px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 0.3em solid black;
+  padding: 30px;
+  border-radius: 5px;
+}
+
+div{
+  margin-bottom: 0.5em;
+
+}
+
 </style>
+
+
